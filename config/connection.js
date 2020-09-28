8cd556eaa6e260954e106a966a56618d4f79237b
David A. Lindner Jr <65383133+davidlindnerjr@users.noreply.github.com>
2020-09-27 18:45:47 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "us-cdbr-east-02.cleardb.com",
   port: 3306,
-  user: "root",
-  password: "jellybeans",
+  user: "b5fc51ef35f10a",
+  password: "c9da2d0c",
   database: "burgers_db"
 });
 
